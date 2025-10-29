5f722f99756ce81c500f0c1c9a20271e15232047
Kamonrat Tudkaew <Hallykmr@Hallykmr.local>
2025-10-30 01:13:44 +0700

    new connect

--- src/env.js
+++ src/env.js
@@ -1,2 +1,2 @@
-export const URL = "https://backend-production-bb8d.up.railway.app"
+export const URL = "https://backend-production-5ab5.up.railway.app"
 // export const URL = "http://localhost:5000"
